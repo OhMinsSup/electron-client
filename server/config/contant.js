386a0c6199083fe7f30a04a89b16eb1ae9ed1af4
OhMinSeop <mins5190@gmail.com>
2021-01-21 23:22:20 +0900

    ADD: redirect url 변경

--- server/config/contant.js
+++ server/config/contant.js
@@ -1,4 +1,4 @@
 module.exports = {
     ZOOM_API: 'https://api.zoom.us/v2',
-    CLIENT_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : ''
+    CLIENT_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://zoom-sdk.netlify.app'
 }
