f32151c78c09dd237380fad0fc3b7e653c06ef96
오민섭 <ominseob@ominseob-ui-iMac.local>
2021-01-22 13:27:32 +0900

    MOD: session store 변경

--- index.server.js
+++ index.server.js
@@ -74,7 +74,10 @@ app.use(
     saveUninitialized: true, // 세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
     cookie: {
       maxAge: 31536000,
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
     },
+    proxy: true,
     store:
       process.env.NODE_ENV === 'production'
         ? new Store({
